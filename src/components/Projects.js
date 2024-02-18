c273f2791170cb6673bfd2470eef41cd23c4e25e
austinp <auspendleton@gmail.com>
2024-02-18 13:12:06 -0700

    Projects styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,15 +8,18 @@ const Projects = () => {
       </div>
       <div className="projects__wtwr-paragraph">
         <p className="projects__wtwr-paragraph-text">
-          With the guidance of my bootcamp, we created this app called What To
-          Wear. This app lets the user add clothing items and classify the
-          weather type that is most appropriate for the clothing item. Then
-          bases on the users location, the app is able to gather their current
-          weather conditions and reccommend what to wear using the clothing
-          items they added. We used react code, created an API, wrote backend
-          code using Express, and deployed it via Google Cloud.
+          An app to help you decide what to wear based on current weather
+          conditions in your area.
         </p>
       </div>
+      <ul className="wtwr__list">
+        <li className="wtwr__list-item">React</li>
+        <li className="wtwr__list-item">JavaScript</li>
+        <li className="wtwr__list-item">HTML</li>
+        <li className="wtwr__list-item">CSS</li>
+        <li className="wtwr__list-item">API</li>
+        <li className="wtwr__list-item">Google Cloud</li>
+      </ul>
       <div className="wtwr__container">
         <h1 className="image__title">What To Wear</h1>
         <img className="wtwr__image" src={wtwr} alt="WTWR app" />
