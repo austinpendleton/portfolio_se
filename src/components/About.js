dc4674fd08785111dd40753be0b97cf460718c06
austinp <auspendleton@gmail.com>
2024-03-05 16:52:42 -0700

    Picture edited

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import osu from "../images/osu-logo.png";
 import physiq from "../images/physiq.png";
 import seahawks from "../images/seahawks-logo.png";
 
-import fenway from "../images/fenway-edited.png";
+import fenway from "../images/fenway-edited.jpeg";
 const About = () => {
   return (
     <section className="about">
