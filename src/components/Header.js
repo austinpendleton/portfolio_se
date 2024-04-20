090ff6fc7f3af18fac6a0a67364306c27aabc386
austinp <auspendleton@gmail.com>
2024-04-19 20:17:37 -0600

    Started Mobile Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,35 +13,8 @@ import github from "../images/github-logo.svg";
 import gmail from "../images/gmail-logo.svg";
 import aplogo from "../images/ap-logo.svg";
 const Header = () => {
-  const [shrink, setShrink] = useState(false);
-  const location = useLocation();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
-
-      // Determine the scroll threshold to trigger shrinking
-      const scrollThreshold = 60;
-
-      // Toggle the shrink state based on the scroll position
-      if (scrollTop > scrollThreshold) {
-        setShrink(true);
-      } else {
-        setShrink(false);
-      }
-    };
-
-    // Add event listener for scroll
-    window.addEventListener("scroll", handleScroll);
-
-    // Remove event listener on component unmount
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [location]); // Re-run effect when location changes
-
   return (
-    <header className={`header ${shrink ? "shrink" : ""}`}>
+    <header className="header">
       <a className="header__logo" href="#intro">
         <img className="header__logo-image" src={aplogo} alt="AP" />
       </a>
@@ -78,13 +51,6 @@ const Header = () => {
           target="_blank"
         >
           <img className="github__logo" alt="GitHub" src={github} />
-        </a>
-        <a
-          className=""
-          href="https://www.instagram.com/austinpendleton/"
-          target="_blank"
-        >
-          <img className="instagram__logo" alt="Instagram" src={instagram} />
         </a>
         <div className="resume__button-container">
           <button className="resume__button">Download Resume</button>
